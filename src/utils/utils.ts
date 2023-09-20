b389ea8f25c1503cb2aaa354386409a01ad800a0
Millifboto <milifboto@gmail.com>
2023-09-20 19:59:57 +0200

    App inicialization

---
+++ src/utils/utils.ts
@@ -0,0 +1,177 @@
+interface ProjectsType {
+  id: number
+  title: string
+  subtitle: string
+  image: ImageType
+  video?: string
+  github: string
+  url: string
+  description: string
+  technologies: TechnologiesType[]
+}
+
+interface ImageType {
+  cover: string
+  secondImage: string
+  thirdImage: string
+}
+
+type TechnologiesType = {
+  name: string
+  svg: string
+}
+
+const Proyects: ProjectsType[] = [
+  {
+    id: 1,
+    title: 'Bookbuster',
+    subtitle: 'Web app',
+    image: {
+      cover: '../../assets/bbt.png',
+      secondImage: '',
+      thirdImage: '',
+    },
+    video: '',
+    github: 'https://github.com/bookbuster-ar/web-app-front',
+    url: '',
+    description: 'https://github.com/bookbuster-ar/web-app-backend',
+    technologies: [
+      {
+        name: 'Javascript',
+        svg: '',
+      },
+      {
+        name: 'Node.js',
+        svg: '',
+      },
+      {
+        name: 'Express',
+        svg: '',
+      },
+      {
+        name: 'PostgreSQL',
+        svg: '',
+      },
+      {
+        name: 'React',
+        svg: '',
+      },
+      {
+        name: 'Redux Toolkit',
+        svg: '',
+      },
+      {
+        name: 'Tailwind',
+        svg: '',
+      },
+      {
+        name: 'Firebase',
+        svg: '',
+      },
+      {
+        name: 'Cloudinary',
+        svg: '',
+      },
+      {
+        name: 'Nodemailer',
+        svg: '',
+      },
+      {
+        name: 'Mercado Pago',
+        svg: '',
+      },
+      {
+        name: 'Sequelize',
+        svg: '',
+      },
+      {
+        name: 'GitHub',
+        svg: '',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Rick And Morty WIKI',
+    subtitle: 'Web app using Typescript',
+    image: {
+      cover: '',
+      secondImage: '',
+      thirdImage: '',
+    },
+    video: '',
+    github: 'https://github.com/Milifboto/RickAndMortyWiki',
+    url: '',
+    description: '',
+    technologies: [
+      {
+        name: 'Typescript',
+        svg: '',
+      },
+      {
+        name: 'Node.js',
+        svg: '',
+      },
+      {
+        name: 'React',
+        svg: '',
+      },
+      {
+        name: 'Redux Toolkit',
+        svg: '',
+      },
+      {
+        name: 'Tailwind',
+        svg: '',
+      },
+      {
+        name: 'Playwright',
+        svg: '',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Woofys',
+    subtitle: 'Web app',
+    image: {
+      cover: '',
+      secondImage: '',
+      thirdImage: '',
+    },
+    video: '',
+    github: 'https://github.com/Milifboto/PIDogs',
+    url: '',
+    description: '',
+    technologies: [
+      {
+        name: 'Javascript',
+        svg: '',
+      },
+      {
+        name: 'Node.js',
+        svg: '',
+      },
+      {
+        name: 'Express',
+        svg: '',
+      },
+      {
+        name: 'PostgreSQL',
+        svg: '',
+      },
+      {
+        name: 'React',
+        svg: '',
+      },
+      {
+        name: 'Redux',
+        svg: '',
+      },
+      {
+        name: 'Sequelize',
+        svg: '',
+      },
+    ],
+  },
+]
